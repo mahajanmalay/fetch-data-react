daa32e0ee9c53f6ba9f11d7c1c1d127effdfa865
Malay Mahajan <98592788+mahajanmalay@users.noreply.github.com>
2023-02-08 11:16:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const getData = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    setUsers(await response.json())
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="box">
+        <table className='table'>
+          <thead>
+            <tr>
+              <th scope='col'></th>
+              <th scope='col'>Contact</th>
+              <th scope='col'>city</th>
+              <th scope='col'>state</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map(curElem =>(
+                <tr>
+                  <td>{curElem.username}</td>
+                  <td>{curElem.name}</td>
+                  <td>{curElem.address.city}</td>
+                  <td>{curElem.address.street}</td>
+                  <td><button>View Details</button></td>
+                </tr>
+
+            ))}
+              
+          </tbody>
+        </table>
+        </div>
+      </div>
+    </>
   );
 }
 
